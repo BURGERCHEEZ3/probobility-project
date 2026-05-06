df65cfee5d17727c1fa83cb63633332b1b923c30
BURGERCHEEZ3 <burgercheez321@gmail.com>
2026-05-06 17:35:34 +0000

    bdfbdfdgrg

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ function guess() {
     return wholeNumber;
 }
 
+
 function handleButtonClick() {
 
     let result = guess();
     let userInput = prompt("Type your guess:");
     alert("Your random number is: " + result);
     if (result) {
-        (userInput == result) {
-        println("Your guess is correct!");
-    else{
-        println("Your guess is incorrect. Try again!");
-    }    
+        (userInput == result) 
+    if (userInput != result) console.log("Your guess is incorrect. Try again!");
+    if (userInput == result) console.log("Your guess is correct!"); 
 
-    }
+    let resultElement = document.getElementById("result");
+
+    if (userInput == result) {
+    resultElement.innerText = "you got it right!";
+    resultElement.style.fontSize = "100px";
+    resultElement.style.textAlign = "center";
+} else {
+    resultElement.innerText = "you got it wrong";
+     resultElement.style.fontSize = "100px";
+     resultElement.style.textAlign = "center";
+}
+
+    
  }
 }
 
