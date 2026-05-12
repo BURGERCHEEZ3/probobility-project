f3776a9b85d81ec12e438c0295edece016c9b2b5
BURGERCHEEZ3 <burgercheez321@gmail.com>
2026-05-12 18:36:59 +0000

    tyrtyhythtyhyth

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 console.log("script started");
 
-function guess() {
-    let randomNumber = Math.random() * 5;
-    let wholeNumber = Math.floor(randomNumber) + 1; 
-    return wholeNumber;
+let topButton = document.getElementById("top-button");
+topButton.addEventListener("click", generateRandomNumber);
+let messageDiv = document.getElementById("message");
+
+let box = document.getElementById("button-box");
+let randomNum = -1;
+
+let restartButton = document.getElementById("restart-button");
+
+restartButton.addEventListener("click", restartGame);
+
+function restartGame() {
+    location.reload();
 }
 
+function generateRandomNumber() {
+  randomNum = Math.random() * 1000;
+  randomNum = Math.floor(randomNum) + 1;
+  console.log("Random number: " + randomNum);
+  makeButtons(1000);
+}
+
+
+function makeButtons(num) {
+  for (let i = 1; i <= num; i++) {
+    let newButton = document.createElement("button");
+    newButton.innerText = i;
+    box.appendChild(newButton);
+      newButton.addEventListener("click", handleButtonClick);
+    
+    box.appendChild(newButton);
+  }
+}
 
 function handleButtonClick() {
-
-    let result = guess();
-    let userInput = prompt("Type your guess:");
-    alert("Your random number is: " + result);
-    if (result) {
-        (userInput == result) 
-    if (userInput != result) console.log("Your guess is incorrect. Try again!");
-    if (userInput == result) console.log("Your guess is correct!"); 
-
-    let resultElement = document.getElementById("result");
-
-    if (userInput == result) {
-    resultElement.innerText = "you got it right!";
-    resultElement.style.fontSize = "100px";
-    resultElement.style.textAlign = "center";
-} else {
-    resultElement.innerText = "you got it wrong";
-     resultElement.style.fontSize = "100px";
-     resultElement.style.textAlign = "center";
+    let newButton = this.innerText;
+    console.log("Button " +  newButton + " was clicked!");
 }
 
-    
- }
-}
+function handleButtonClick() {
+    let newButton = this.innerText;
 
-let myButton = document.getElementById("randomButton");
-myButton.addEventListener("click", handleButtonClick);
+ if (newButton == randomNum) {
+          messageDiv.innerText = "Congratulations, you got the button!";
+          this.style.backgroundColor = "yellow";
+    } else if (newButton < randomNum) {
+        messageDiv.innerText = "Higher! You clicked " + newButton + ".";
+        this.style.backgroundColor = "red";
+    } else {
+        messageDiv.innerText = "Lower! You clicked " + newButton + ".";
+        this.style.backgroundColor = "blue";
+    }
+}
